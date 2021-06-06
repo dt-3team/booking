f3ee6bc5dd9d381a80093e350c641cc2cb981731
Yuhan <hanseok.yu@m2utech.com>
2021-06-06 23:56:26 +0900

    Login 추가

--- src/main/java/anticorona/PolicyHandler.java
+++ src/main/java/anticorona/PolicyHandler.java
@@ -1,6 +1,9 @@
 package anticorona;
 
 import anticorona.config.kafka.KafkaProcessor;
+
+import java.util.Optional;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+    
     @Autowired BookingRepository bookingRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
@@ -18,10 +22,12 @@ public class PolicyHandler{
         if(!completed.validate()) return;
 
         System.out.println("\n\n##### listener UpdateStatus : " + completed.toJson() + "\n\n");
-
-        // Sample Logic //
-        Booking booking = new Booking();
-        bookingRepository.save(booking);
+        Optional<Booking> booking = bookingRepository.findById(completed.getBookingId());
+        if(booking.isPresent()){
+            Booking bookingValue = booking.get();
+            bookingValue.setStatus("INJECTION_COMPLETED");
+            bookingRepository.save(bookingValue);
+        }
             
     }
 
